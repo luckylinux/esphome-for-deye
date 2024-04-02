f6445fa9c5a04c52bef0be5dd886db732279264f
luckylinux <luckylinux>
2024-04-02 15:14:05 +0200

    Adding type master/slave in device config

--- devices.sh
+++ devices.sh
@@ -4,16 +4,20 @@
 names=()
 macs=()
 protocols=()
+types=()
 
 # Populate device list
 # Inverter M01 (Master)
 names[0]="inverter/deye-01"
+types[0]="master"
 
 # Inverter S02 (Slave)
 names[1]="inverter/deye-02"
+types[1]="slave"
 
 # Inverter S03 (Slave)
 names[2]="inverter/deye-03"
+types[2]="slave"
 
 # Get nzumber of devices
 num=${#names[@]}
@@ -25,10 +29,12 @@ do
     for ((index=0;index<=$maxindex;index++))
     do
         name=${names[$index]}
+        type=${types[$index]}
 
         selection=$((index+1))
         echo -e "[${selection}]"
         echo -e "\t Hostname: ${name}"
+        echo -e "\t Type: ${type}"
     done
 
     read -p "Enter desired configuration: " selection
@@ -37,3 +43,4 @@ do
 done
 
 echo "Hostname set to <$name>"
+echo "Type is <$type>"
